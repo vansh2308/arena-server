7cd0431e0017034bd6480dba985547166aa7b88e
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-12-07 04:58:59 +0530

    Connect local mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const configOptions = {
+  isGlobal: true,
+  envFilePath: ['.env.development', '.env']
+}
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(configOptions),
+    TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+      
+        // WIP: Add parseInt to env 
+        port: 3306,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DB,
+        entities: [],
+        synchronize: true,
+      }
+      ),
+  ],
+  
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
